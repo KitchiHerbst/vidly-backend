bea3a123f56115fbe6e26d537f3f9ac945782060
Triston <tristonherbst97@gmail.com>
2021-07-30 14:30:40 -0500

    genres array created

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const express = require('express')
 // this sets up our express app allowing us to use get, put, post, delete
 const app = express()
 
+let genres = [
+    {
+        id: 1,
+    name: 'horror'
+},{
+    id: 2,
+name: 'comedy'
+},{
+    id: 3,
+name: 'thriller'
+},{
+    id: 4,
+name: 'romance'
+}
+]
+
 app.get('/', (req, res) => {
     res.send('bingo bongo')
 })
