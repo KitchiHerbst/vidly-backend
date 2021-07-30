d00e93a4fa490d1f338a0cd215924d8e66943425
Triston <tristonherbst97@gmail.com>
2021-07-30 14:32:05 -0500

    route created to see genres

--- index.js
+++ index.js
@@ -24,5 +24,9 @@ app.get('/', (req, res) => {
     res.send('bingo bongo')
 })
 
+app.get('/api/genres', (req, res) => {
+    res.send(genres)
+})
+
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listening on ${port}`))
