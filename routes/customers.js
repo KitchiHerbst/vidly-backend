e456cae47199b49f9cc3f3a86be63a1e25f70217
Triston <tristonherbst97@gmail.com>
2021-08-11 20:28:43 -0500

    post and put

--- routes/customers.js
+++ routes/customers.js
@@ -27,4 +27,31 @@ router.get("/", async (req, res) => {
   res.send(customers);
 });
 
+router.get("/:id", async (req, res) => {
+    const customer = await Customer.findById(req.params.id)
+    res.send(customer)
+})
+
+router.post("/", async (req, res) => {
+    const newCustomer = new Customer ({
+        name: req.body.name,
+        isGold: req.body.isGold,
+        phone: req.body.phone
+    })
+
+    const result = await newCustomer.save()
+    res.send(result)
+})
+
+router.put("/:id", async (req, res) => {
+    const customer = await Customer.findById(req.params.id)
+    const updatedCustomer = customer.set({
+        name: req.body.name,
+        isGold: req.body.isGold,
+        phone: req.body.phone
+    })
+    const result = await updatedCustomer.save()
+    res.send(result)
+})
+
 module.exports = router
