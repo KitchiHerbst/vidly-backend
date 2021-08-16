ee22e34bf10fbceae31f1dd10226612df6dcaaca
Triston <tristonherbst97@gmail.com>
2021-08-16 09:48:18 -0500

    headers when creating account

--- index.js
+++ index.js
@@ -4,44 +4,48 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const debug = require("debug")("app:startup");
 const Joi = require("joi");
-Joi.objectId = require('joi-objectid')(Joi)
+Joi.objectId = require("joi-objectid")(Joi);
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //local imports
-const genres = require('./routes/genres')
-const home = require('./routes/home')
-const customers = require('./routes/customers');
-const rentals = require('./routes/rentals')
-const users = require("./routes/users")
-const auth = require("./routes/auth")
+const genres = require("./routes/genres");
+const home = require("./routes/home");
+const customers = require("./routes/customers");
+const rentals = require("./routes/rentals");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
 const { custom } = require("joi");
-const movies = require("./routes/movies")
+const movies = require("./routes/movies");
+const config = require("config");
+
+if (!config.get("jwtPrivateKey")) {
+  console.log("FATAL ERROR: jwt is not defined");
+  process.exit(1);
+}
 
 // this sets up our express app
 const app = express();
-mongoose.connect('mongodb://localhost/vidly')
-  .then(() => console.log('Connected to MongoDB...') )
-  .catch(() => console.error('Cant connect to mongodb'))
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(() => console.error("Cant connect to mongodb"));
 
 if (app.get("env") === "development") {
   app.use(morgan("tiny"));
-  debug('Morgan Enabled')
+  debug("Morgan Enabled");
 }
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.use('/api/genres', genres)
-app.use('/api/users', users)
-app.use('/api/customers', customers)
-app.use('/api/movies', movies)
-app.use('/api/rentals', rentals)
-app.use('/api/auth', auth)
-app.use('/', home)
-
+app.use("/api/genres", genres);
+app.use("/api/users", users);
+app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
+app.use("/api/auth", auth);
+app.use("/", home);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on ${port}`));
-
-
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
+const jwt = require('jsonwebtoken')
 const bcrypt = require("bcrypt");
 const express = require("express");
 const Joi = require("joi");
 const router = express.Router()
 const _ = require("lodash")
+const config = require('config')
 
 const {User} = require("../models/user")
 
@@ -16,7 +18,9 @@ router.post('/', async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password)
     if(!validPassword) return res.status(400).send('Invalid email and password')
 
-    res.send(true)
+    const token = jwt.sign({_id: user._id}, config.get('jwtPrivateKey'))
+
+    res.send(token)
 })
 
 const validate = ({email, password}) => {
